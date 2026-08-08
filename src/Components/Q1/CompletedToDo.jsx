f735f85d47f406c3fe561d69ef2aaec5d2bb02a9
Ishita <ishita.iit.sharma@gmail.com>
2026-08-08 20:24:00 +0530

    Q1

---
+++ src/Components/Q1/CompletedToDo.jsx
@@ -0,0 +1,18 @@
+import React,{useEffect,useState} from 'react';
+
+const CompletedToDo = () => {
+  const[apiData,setApiData]=useState([]);
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos")
+    .then((res)=>{
+
+    })
+  },[]
+);
+  return (
+    
+    <div>CompletedToDo</div>
+  )
+}
+
+export default CompletedToDo
